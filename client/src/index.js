90390982d29b34baae7b98831b600c2ec5facad4
Thomas <at93om@gmail.com>
2019-04-21 17:02:01 -0400

    mern done

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
+// import registerServiceWorker from "./registerServiceWorker";
 import './index.css';
 import App from './App';
 
