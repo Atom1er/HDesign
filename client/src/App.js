50ac48861bfe0d79adbcb96a9b70649cc3ffd063
unknown <rnakada90@gmail.com>
2019-04-25 19:15:45 -0400

    updating

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home';
+import Home from './pages/Home';
 import Admin from './components/Admin';
 import SignUp from './components/SignUp';
 import LogIn from './components/LogIn';
@@ -12,6 +12,8 @@ import PhotoWidgetUploader from './components/PhotoWidgetUploader';
 import UsersDb from './components/UsersDb';
 import Navy from './components/Navy';
 
+import Modern from "./pages/Modern"
+
 
 
 class App extends Component {
@@ -19,22 +21,24 @@ class App extends Component {
     const App = () => (
       <div>
         <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route exact path='/admin' component={Admin}/>
-          <Route path='/list' component={List}/>
-          <Route exact path='/login' component={LogIn}/>
-          <Route path='/signup' component={SignUp}/>
-          <Route path='/navy' component={Navy}/>
-           <Route exact path="/photoWidgetUploader" component={PhotoWidgetUploader}/>
-          <Route exact path="/apiPhotos" component={ApiPhotos}/>
-          <Route exact path="/usersDb" component={UsersDb}/>
+          <Route exact path='/' component={Home} />
+          <Route exact path="/modern" component={Modern} />
+
+          <Route exact path='/admin' component={Admin} />
+          <Route path='/list' component={List} />
+          <Route exact path='/login' component={LogIn} />
+          <Route path='/signup' component={SignUp} />
+          <Route path='/navy' component={Navy} />
+          <Route exact path="/photoWidgetUploader" component={PhotoWidgetUploader} />
+          <Route exact path="/apiPhotos" component={ApiPhotos} />
+          <Route exact path="/usersDb" component={UsersDb} />
         </Switch>
       </div>
     )
 
     return (
       <Switch>
-        <App/>
+        <App />
       </Switch>
     );
   }
---
+++ client/src/pages/Modern.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Modern = props => (
+
+<div>
+
+<h1>Modern</h1>
+
+</div>
+
+)
+
+export default Modern;
